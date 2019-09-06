542c696983f62c30683d11e4fc62c3005dbf88da
¨siranjivi-it19¨ <siranjivi.it19@bitsathy.ac.in>
2019-09-06 12:02:02 +0530

    first commit

---
+++ natural.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+int n=5;
+printf("n=5\n");
+printf("%d",((n*(n-1)/2)));
+}
---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a=6,b=b=2;
+printf("a=6\nb=2\n");
+printf("sum=%d\n",a+b);
+printf("minus=%d\n",a-b);
+printf("multiply=%d\n",a*b);
+printf("divide=%d\n",a/b);
+}
